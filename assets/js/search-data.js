86bc67d628f7ea0f21d3893a58a2c01f98d88d33
brookebao <158602782+brookebao@users.noreply.github.com>
2025-04-09 05:26:58 +0000

    Deploying to gh-pages from @ baobrooke/baobrooke.github.io@5f5c5d3cb40a6297a7d347e93c2146621478c63a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-cs-299-class-to-present-at-ruhlman-4-26-2025",
+          title: 'CS 299 Class to present at Ruhlman 4/26/2025.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',

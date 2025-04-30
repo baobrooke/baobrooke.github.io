8ad21a64ba3f518e9e2d3bb76109c5c936be9b99
brookebao <158602782+brookebao@users.noreply.github.com>
2025-04-30 00:49:18 +0000

    Deploying to gh-pages from @ baobrooke/baobrooke.github.io@64d505a08ef5e2f0d6e1fa071e7d884b67e76fa7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,8 +51,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
+        },{id: "nav-people1",
+          title: "people1",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {

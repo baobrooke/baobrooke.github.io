356534b4b85e8b853a79dcac0b29dea39914d92f
brookebao <158602782+brookebao@users.noreply.github.com>
2025-06-01 10:04:38 +0000

    Deploying to gh-pages from @ baobrooke/baobrooke.github.io@0c3e971de2826a60d74e0a053d5da411dc75fec6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,8 +24,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "Progress on current and past projects can be found here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
